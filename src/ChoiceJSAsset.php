af2ac593839720ba221bb6148634d6223ca6f924
egmsystems <GitHub@EGMSystems.net>
2022-05-25 20:06:29 -0500

    Update ChoiceJSAsset.php

--- src/ChoiceJSAsset.php
+++ src/ChoiceJSAsset.php
@@ -9,7 +9,7 @@ class ChoiceJSAsset extends \yii\web\AssetBundle{
     /**
      * {@inheritdoc}
      */
-    public $sourcePath = __DIR__ . '/src';
+    public $sourcePath = '@runtime/choiceJSAsset';
     /**
      * {@inheritdoc}
      */
@@ -28,4 +28,27 @@ class ChoiceJSAsset extends \yii\web\AssetBundle{
     public $css = [
         'styles/choices' . (YII_ENV_PROD ? ".min" : "") . '.css',
     ];
+    function init() {
+        parent::init();
+        if(!is_dir($runtime = \Yii::getAlias($this->sourcePath))){
+            if(!mkdir($runtime)){
+                throw new Exception("!mkdir($runtime)");
+            }
+        }
+        foreach (['scripts/choices', 'styles/choices'] as $asset) {
+            foreach (['', '.min'] as $min) {
+                if(strpos($asset, "scripts") === 0){
+                    $ext = "js";
+                }else{
+                    $ext = "css";
+                }
+                if(!file_exists($runtime . $tmp = "$value$min.$ext")){
+                    $file_get_contents = file_get_contents("https://cdn.jsdelivr.net/npm/choices.js/public/assets/$tmp");
+                    if(file_put_contents($runtime . $tmp, $file_get_contents) === false){
+                        throw new Exception("!file_put_contents($runtime$tmp, file_get_contents)");
+                    }
+                }
+            }
+        }
+    }
 }
